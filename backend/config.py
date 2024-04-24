0e4e2d876b34ec60c05d166b8fd64ce1cd171b70
Serafim Oleynik <serafim.oleynik.5777@yandex.ru>
2024-04-24 21:57:50 +0300

    add: Change database from SQLite to PostgrSQL in docker-compose

---
+++ backend/config.py
@@ -0,0 +1,15 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # loads the configs from .env
+
+
+class Settings:
+    DB_HOST = os.getenv("DB_HOST")
+    DB_PORT = os.getenv("DB_PORT")
+    DB_USER = os.getenv("DB_USER")
+    DB_PASS = os.getenv("DB_PASS")
+    DB_NAME = os.getenv("DB_NAME")
+
+
+settings = Settings()
