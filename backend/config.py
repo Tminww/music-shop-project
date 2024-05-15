f5d8e4ffad010a3ca655f5b192d6abf25425314c
Serafim Oleynik <serafim.oleynik.5777@yandex.ru>
2024-05-15 21:53:18 +0300

    add: api_key

--- backend/config.py
+++ backend/config.py
@@ -10,6 +10,7 @@ class Settings:
     DB_USER = os.getenv("DB_USER")
     DB_PASS = os.getenv("DB_PASS")
     DB_NAME = os.getenv("DB_NAME")
+    API_KEY = os.getenv("API_KEY")
 
 
 settings = Settings()
